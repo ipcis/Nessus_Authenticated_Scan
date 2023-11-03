759f26120aa247c062ad09501231578376c2d910
Core|Threat <30553333+ipcis@users.noreply.github.com>
2023-11-03 10:23:27 +0100

    Update check_state.ps1

--- check_state.ps1
+++ check_state.ps1
@@ -1,13 +1,27 @@
-$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
-$registryValueName = "LocalAccountTokenFilterPolicy"
+# Überprüfen des ersten Registry-Schlüssels und Werts
+$registryPath1 = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+$registryValueName1 = "SmbServerNameHardeningLevel"
 
-$registryValue = Get-ItemProperty -Path $registryPath -Name $registryValueName
+$registryValue1 = Get-ItemProperty -Path $registryPath1 -Name $registryValueName1
 
-if ($registryValue.$registryValueName -eq 1) {
-    Write-Host "Der Registry-Wert $registryValueName ist auf 1 gesetzt."
+# Überprüfen des zweiten Registry-Schlüssels und Werts
+$registryPath2 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$registryValueName2 = "LocalAccountTokenFilterPolicy"
+
+$registryValue2 = Get-ItemProperty -Path $registryPath2 -Name $registryValueName2
+
+if ($registryValue1.$registryValueName1 -eq 1) {
+    Write-Host "Der Registry-Wert $registryValueName1 ist auf 1 gesetzt."
 } else {
-    Write-Host "Der Registry-Wert $registryValueName ist nicht auf 1 gesetzt oder nicht vorhanden."
+    Write-Host "Der Registry-Wert $registryValueName1 ist nicht auf 1 gesetzt oder nicht vorhanden."
 }
+
+if ($registryValue2.$registryValueName2 -eq 1) {
+    Write-Host "Der Registry-Wert $registryValueName2 ist auf 1 gesetzt."
+} else {
+    Write-Host "Der Registry-Wert $registryValueName2 ist nicht auf 1 gesetzt oder nicht vorhanden."
+}
+
 
 
 
